61ce081a4e9d4be35252463eab0053d584380e29
DaehyeonKim <Daehyeonoff@gmail.com>
2022-05-04 22:38:59 +0900

    20220504

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React, {useState} from 'react';
 import './App.css';
 import TodoBack from './components/TodoBack';
+import TodoList from './components/TodoList';
 
 
 function App() {
+  const {todos, setTodos} = useState([
+    {
+      id:1,
+      text:"할일 1",
+      checked:true,
+    },
+    {
+      id:2,
+      text:"할일 2",
+      checked:false,
+    },
+    {
+      id:3,
+      text:"할일 3",
+      checked:true,
+    },
+  ])
   return (
-      <TodoBack>안녕하세요</TodoBack>
+      <TodoBack>
+        <TodoList />
+      </TodoBack>
   );
 }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList = ({todos}) => {
+    return (
+        <div>
+            {todos.map(<div>{todos.text}</div>)}
+        </div>
+    )
+}
+
+export default TodoList;
